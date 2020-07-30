ea9508851d685611f8399349e61838e051096b37
Graph-Donte-Crypto <Dimasik841@gmail.com>
2020-07-30 23:32:14 +0300

    Span update

--- Templates/Span.hpp
+++ Templates/Span.hpp
@@ -13,22 +13,32 @@ namespace uft {
 	struct Span {
 		const Type * ptr = nullptr;
 		size_t length = 0;
+		bool source = false;
 		
-		void set(const Type * ptr, size_t length) {
+		void set(const Type * ptr, size_t length, bool source = false) {
 			this->ptr = ptr;
 			this->length = length;
+			this->source = source;
 		}
 		
 		Span(const Span & span) {
-			set(span.ptr, span.length);
+			//Если в конструктор был передан оригинальный Span - новый Span оригинальный, старый - нет
+			//Иначе оба не оригинальные
+			set(span.ptr, span.length, span.source);
+			if (span.source) span.source = false;
 		}
 		
-		Span(const Type * ptr, size_t length) {
-			set(ptr, length);
+		Span(const Type * ptr, size_t length, bool source = false) {
+			set(ptr, length, source);
 		}
 				
 		Span() {
-			set(nullptr, 0);
+			//По умолчанию все Span - не оригинальные
+			set(nullptr, 0, false);
+		}
+		~Span() {
+			//Если Span - оригинальный, то он освобождает память при удалении
+			if (source) delete[] ptr;
 		}
 		
 		const Type & operator [] (size_t index) const {return ptr[index];}
