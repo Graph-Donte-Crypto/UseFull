161a858700a89ed5a1d5a1ddabfe7a2fa911d1a9
Graph-Donte-Crypto <Dimasik841@gmail.com>
2020-07-31 05:28:31 +0300

    Span hot fix & some comments

--- Templates/Span.hpp
+++ Templates/Span.hpp
@@ -6,39 +6,76 @@
 //Make by Graph Don'te-Crypto
 
 #include "../Utils/Concepts.hpp"
+#include "../Utils/Macro.hpp"
+#include <cstddef>
+#include <stdio.h>
 
 namespace uft {
+	
+	using namespace utils;
+	
 	//Readonly array
 	template <typename Type>
 	struct Span {
 		const Type * ptr = nullptr;
 		size_t length = 0;
-		bool source = false;
+		bool owner = false;
 		
-		void set(const Type * ptr, size_t length, bool source = false) {
+		void set(const Type * ptr, size_t length, bool owner = false) {
 			this->ptr = ptr;
 			this->length = length;
-			this->source = source;
+			this->owner = owner;
 		}
 		
-		Span(const Span & span) {
-			//Если в конструктор был передан оригинальный Span - новый Span оригинальный, старый - нет
-			//Иначе оба не оригинальные
-			set(span.ptr, span.length, span.source);
-			if (span.source) span.source = false;
+		/*
+		
+		ptr_cast - это очень плохо
+		Особенно для действия вида const Type -> Type
+		Но я не помню, как сделать функцию, которая принимает rvalue
+		Поэтому пока тут ptr_cast
+		
+		*/
+		
+		/*
+		//Если был передан оригинальный Span - новый Span оригинальный, старый - нет
+		//Иначе оба не оригинальные
+		*/
+		
+		//TODO: REMOVE ptr_cast && add rvalue variants
+		Span(const Span<Type> & span) {
+			
+			set(span.ptr, span.length, span.owner);
+			if (span.owner) ptr_cast(Span<Type>, span).owner = false;
 		}
 		
-		Span(const Type * ptr, size_t length, bool source = false) {
-			set(ptr, length, source);
+		Span<Type> & operator = (const Span<Type> & span) {
+			if (owner) deleteData();
+			set(span.ptr, span.length, span.owner);
+			if (span.owner) ptr_cast(Span<Type>, span).owner = false;
+			return *this;
+		}
+		//
+		
+		Span(const Type * ptr, size_t length, bool owner = false) {
+			set(ptr, length, owner);
 		}
 				
 		Span() {
 			//По умолчанию все Span - не оригинальные
 			set(nullptr, 0, false);
 		}
+		
+		void deleteData() {
+			delete[] ptr;
+			ptr = nullptr;
+			length = 0;
+			owner = false;
+			printf("Deleted\n");
+		}
+		
 		~Span() {
 			//Если Span - оригинальный, то он освобождает память при удалении
-			if (source) delete[] ptr;
+			if (owner) deleteData();
 		}
 		
 		const Type & operator [] (size_t index) const {return ptr[index];}
@@ -47,6 +84,8 @@ namespace uft {
 			for (size_t i = 0; i < length; i++)
 				lambda(ptr[i]);
 		}
+		
+		//Unsafe на то и Unsafe, чтобы была дыра в безопасности и возможность изменять данные в Span
 		
 		Type & getUnsafe(size_t index) {return ((Type *)ptr)[index];}
 	};
