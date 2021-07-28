b075dbef6ecbc0dd97f2e2b527f3e16c2cbd79d9
Graph-Donte-Crypto <Dimasik841@gmail.com>
2021-07-29 00:46:23 +0300

    Add diagnostic ignore for clang and gcc

--- Utils/StdDiagnosticIgnore.hpp
+++ Utils/StdDiagnosticIgnore.hpp
@@ -1,3 +1,24 @@
+#ifdef __clang__
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wstrict-overflow"
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Weffc++"
+#pragma clang diagnostic ignored "-Wredundant-decls"
+#pragma clang diagnostic ignored "-Wfloat-conversion"
+#pragma clang diagnostic ignored "-Wctor-dtor-privacy"
+#pragma clang diagnostic ignored "-Wfloat-equal"
+#pragma clang diagnostic ignored "-Wcast-qual"
+#pragma clang diagnostic ignored "-Wnon-virtual-dtor"
+#pragma clang diagnostic ignored "-Wexpansion-to-defined"
+#pragma clang diagnostic ignored "-Wpedantic"
+#pragma clang diagnostic ignored "-pedantic"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wexpansion-to-defined"
+
+#else
+
+#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wstrict-overflow"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
 #pragma GCC diagnostic ignored "-Weffc++"
@@ -11,3 +32,5 @@
 #pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wextra"
 #pragma GCC diagnostic ignored "-Wexpansion-to-defined"
+
+#endif
---
+++ Utils/StdDiagnosticIgnoreEnd.hpp
@@ -0,0 +1,9 @@
+#ifdef __clang__
+
+#pragma clang diagnostic pop
+
+#else
+
+#pragma GCC diagnostic pop
+
+#endif
