8f9381cc7ea439abbccea1a894a078f07cdc56b3
Graph-Donte-Crypto <Dimasik841@gmail.com>
2021-08-24 20:43:33 +0300

    Fix tabs

--- String/StringFormatBuilder.hpp
+++ String/StringFormatBuilder.hpp
@@ -19,40 +19,40 @@ namespace str {
 
 		#ifdef RUNTIME_DEBUG
 		void validateSize() {
-            if (pos - buffer > size) {
-                printf("ERROR: StringFormatBuilder<%llu> don't have enough space\n", size);
-                exit(1);
-            }
+			if (pos - buffer > size) {
+				printf("ERROR: StringFormatBuilder<%llu> don't have enough space\n", size);
+				exit(1);
+			}
 		}
 		#endif // RUNTIME_DEBUG
 
 		StringFormatBuilder()
-            : pos(buffer)
-        {}
+			: pos(buffer)
+		{}
 
-        std::string toString() {
-            return std::string(buffer, pos - buffer);
-        }
+		std::string toString() {
+			return std::string(buffer, pos - buffer);
+		}
 
-        template <typename Type>
-        StringFormatBuilder & append(const Type & obj, const char * format) {
-            pos += sprintf(pos, format, obj);
-            DEBUG(
-                validateSize();
-            )
-            return *this;
-        }
+		template <typename Type>
+		StringFormatBuilder & append(const Type & obj, const char * format) {
+			pos += sprintf(pos, format, obj);
+			DEBUG(
+				validateSize();
+			)
+			return *this;
+		}
 
-        StringFormatBuilder & append(int                value, const char * format = "%d"   ) { return append<int>               (value, format);}
-        StringFormatBuilder & append(long               value, const char * format = "%ld"  ) { return append<long>              (value, format);}
-        StringFormatBuilder & append(long long          value, const char * format = "%lld" ) { return append<long long>         (value, format);}
-        StringFormatBuilder & append(unsigned           value, const char * format = "%u"   ) { return append<unsigned>          (value, format);}
-        StringFormatBuilder & append(unsigned long      value, const char * format = "%lu"  ) { return append<unsigned long>     (value, format);}
-        StringFormatBuilder & append(unsigned long long value, const char * format = "%llu" ) { return append<unsigned long long>(value, format);}
-        StringFormatBuilder & append(float              value, const char * format = "%f"   ) { return append<float>             (value, format);}
-        StringFormatBuilder & append(double             value, const char * format = "%lf"  ) { return append<double>            (value, format);}
-        StringFormatBuilder & append(long double        value, const char * format = "%lf"  ) { return append<long double>       (value, format);}
-        StringFormatBuilder & append(const char *       value, const char * format = "%s"   ) { return append<const char *>      (value, format);}
+		StringFormatBuilder & append(int                value, const char * format = "%d"   ) { return append<int>               (value, format);}
+		StringFormatBuilder & append(long               value, const char * format = "%ld"  ) { return append<long>              (value, format);}
+		StringFormatBuilder & append(long long          value, const char * format = "%lld" ) { return append<long long>         (value, format);}
+		StringFormatBuilder & append(unsigned           value, const char * format = "%u"   ) { return append<unsigned>          (value, format);}
+		StringFormatBuilder & append(unsigned long      value, const char * format = "%lu"  ) { return append<unsigned long>     (value, format);}
+		StringFormatBuilder & append(unsigned long long value, const char * format = "%llu" ) { return append<unsigned long long>(value, format);}
+		StringFormatBuilder & append(float              value, const char * format = "%f"   ) { return append<float>             (value, format);}
+		StringFormatBuilder & append(double             value, const char * format = "%lf"  ) { return append<double>            (value, format);}
+		StringFormatBuilder & append(long double        value, const char * format = "%lf"  ) { return append<long double>       (value, format);}
+		StringFormatBuilder & append(const char *       value, const char * format = "%s"   ) { return append<const char *>      (value, format);}
 	};
 
 	template <typename Type>
@@ -61,7 +61,7 @@ namespace str {
 	}
 
 	template <typename Type>
-    static std::string toStringFormat(const Type & obj, const char * format) {
+	static std::string toStringFormat(const Type & obj, const char * format) {
 		return StringFormatBuilder<>().append(obj, format).toString();
 	}
 
